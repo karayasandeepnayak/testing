17bf98fc6ef6182a378829f7453775cf2457987b
eTouch <eTouch@Lenovo-T430>
2014-06-18 13:52:53 -0700

    test

---
+++ Testing/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+	
+	HelloWorld(){
+		System.out.println("Hello World");
+		System.out.println("Object Creater");
+	}
+	
+	public void display(){
+		
+		System.out.println("Hey there how are you!! This class is written for the sole purpose of testing Git!");
+	}
+	
+	public static void main(String[] args){
+		
+		HelloWorld h1 = new HelloWorld();
+	}
+}
